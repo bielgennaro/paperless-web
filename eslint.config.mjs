e3c7b6c63d2246b89494ee6445145a9a5a805b00
bielgennaro <zirondigabriel@gmail.com>
2025-10-25 23:08:42 -0300

    feat: add components.json for configuration and styling
    chore: update eslint configuration to use antfu's style guide
    
    feat: implement utility function for class name merging
    
    chore: update package.json with new dependencies and versions
    
    fix: enhance tsconfig.json for better type checking and path resolution

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,42 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import antfu from '@antfu/eslint-config'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default antfu(
+    {
+        stylistic: {
+            indent: 4,
+            quotes: 'single',
+            semi: false,
+            objectCurlySpacing: true,
+            arrayBracketSpacing: false,
+            arrowParens: 'avoid',
+            trailingComma: 'all',
+            maxLineLength: 120,
+            spaceBeforeFunctionParen: false,
+            bracketSpacing: true,
+            blockSpacing: true,
+            keySpacing: true,
+            commaDangle: 'always-multiline',
+        },
+    },
+    {
+        files: ['**/*.js', '**/*.ts'],
+        rules: {
+            'node/prefer-global/process': 'off',
+            'no-console': 'on',
+            'antfu/no-top-level-await': 'off',
 
-export default eslintConfig;
+            'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+            'style/comma-spacing': ['error', { before: false, after: true }],
+            'style/object-curly-newline': ['error', {
+                multiline: true,
+                consistent: true,
+            }],
+            'style/padding-line-between-statements': [
+                'error',
+                { blankLine: 'always', prev: '*', next: 'return' },
+                { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+                { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+            ],
+        },
+    },
+)
